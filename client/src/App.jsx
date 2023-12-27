b476e38d7633498dc915d8bc0b54ab9fe51a6c6d
onepice0807 <onepice0807@gmail.com>
2023-12-27 10:53:20 +0900

    #3 React-router 초기 설정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
+// App.jsx
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import GlobalStyle from './styles/globalStyles';
+import { AuthPage, DashboardPage, ErrorPage, Root } from './pages';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <DashboardPage /> },
+        { path: 'auth', element: <AuthPage /> }
+      ]
+    }
+  ]);
+
   return (
     <>
       <GlobalStyle />
+      <RouterProvider router={router} />
     </>
   );
 };
